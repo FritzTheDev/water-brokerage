415dfb5662f901ac02eac66db59c7d13dc363a1e
Frederick Johnson <fritzj96@gmail.com>
2020-01-30 08:28:51 -0800

    fixed: login service export

---
+++ src/services/auth.service.ts
@@ -0,0 +1,32 @@
+import { Response, Request, NextFunction } from "express";
+import { getRepository } from "typeorm";
+import * as jwt from "jsonwebtoken";
+import { compare } from "bcrypt";
+
+import { LoginDTO } from "../dtos/login.dto";
+import { User } from "../entities/user.entity";
+import { BadCredentialsException } from "../exception/badCredential.exception";
+
+const createToken = (user: User) => {
+  const id = user.id;
+  return `Bearer ${jwt.sign({ id }, process.env.JWT_SECRET, { expiresIn: 60 * 60 * 24 * 7 })}`;
+}
+
+export const login = async (userCreds: LoginDTO) => {
+  const userRepo = getRepository(User);
+  
+  const user = await userRepo.findOne({ email: userCreds.email });
+  try {
+    if (!(await compare(userCreds.password, user.password))) {
+      throw new BadCredentialsException();
+    }
+  } catch (error) {
+    throw error;
+  }
+  user.password = undefined;
+  return Promise.resolve(createToken(user));
+}
+
+export const registerService = async (userData: CreateUserDto) => {
+
+}
