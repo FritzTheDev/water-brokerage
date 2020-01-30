819e4a2bc2b17e0ab84fe667c6bcc440e3c96fe4
Frederick Johnson <fritzj96@gmail.com>
2020-01-30 07:59:06 -0800

    removed: commented out local strat

--- src/util/configurePassport.ts
+++ src/util/configurePassport.ts
@@ -9,24 +9,31 @@ import { PassportStatic } from "passport";
 import { getRepository } from "typeorm";
 import { User } from "../entities";
 
-const verifyPassword = async (user: User, password: string): Promise<Boolean> =>
-  await compare(password, user.password);
+// const verifyPassword = async (user: User, password: string): Promise<Boolean> =>
+//   await compare(password, user.password);
 
-export const configurePassportLocal = (passport: PassportStatic) => {
-  const userRepository = getRepository(User);
-  passport.use(
-    new LocalStrategy(async (email, password, done) => {
-      try {
-        const user = await userRepository.findOne({ email });
-        if (!user) return done(null, false);
-        if (!(await verifyPassword(user, password))) return done(null, false);
-        return done(null, user);
-      } catch (error) {
-        return done(error);
-      }
-    })
-  );
-};
+// export const configurePassportLocal = (passport: PassportStatic) => {
+//   const userRepository = getRepository(User);
+//   passport.use(
+//     new LocalStrategy({ usernameField: "email" }, (email, password, done) => {
+//       userRepository
+//         .findOne({ email })
+//         .then(user => {
+//           if (!user) return done(null, false);
+//           verifyPassword(user, password)
+//             .then(isValid => {
+//               return isValid ? done(null, user) : done(null, false);
+//             })
+//             .catch(error => {
+//               return done(error);
+//             });
+//         })
+//         .catch(error => {
+//           return done(error);
+//         });
+//     })
+//   );
+// };
 
 export const configurePassportJwt = (passport: PassportStatic) => {
   const options: StrategyOptions = {
