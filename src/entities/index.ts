d84fa9eb501af78c1511d3d5394f7e4baae6cd86
Frederick Johnson <fritzj96@gmail.com>
2020-01-30 06:07:59 -0800

    added: entity index

---
+++ src/entities/index.ts
@@ -0,0 +1,4 @@
+export { Listing } from "./listing.entity";
+export { Offer } from "./offer.entity";
+export { User } from "./user.entity";
+export { WestlandsAccount } from "./westlandsAccount.entity";
