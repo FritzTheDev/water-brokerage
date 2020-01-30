5d3843c4d78ceeaf03a5e2552fce3d71c2cee990
Frederick Johnson <fritzj96@gmail.com>
2020-01-30 08:22:46 -0800

    added: set up index imprort

--- src/routes/auth.routes.ts
+++ src/routes/auth.routes.ts
@@ -1,12 +1,13 @@
-import { Router } from "express";
+import Router from "express-promise-router";
 import { authenticate } from "passport";
-import { login } from "../services/auth.service";
+import { login } from "../controllers";
 import { validationMiddleware } from "../middleware/validation.middleware";
 import { LoginDTO } from "../dtos/login.dto";
 import { CreateUserDto } from "../dtos/createUser.dto";
+
 const router = Router();
 
-router.post('/login',  validationMiddleware(LoginDTO), authenticate('local'), login);
-router.post('/register', validationMiddleware(CreateUserDto), )
+router.post('/login',  validationMiddleware(LoginDTO), login);
+router.post('/register', validationMiddleware(CreateUserDto),  )
 
 export { router as authRoutes };
