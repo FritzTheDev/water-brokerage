84cbbc0ca8ceca9ed5f0b911eaf8fa4087f77ad6
Frederick Johnson <fritzj96@gmail.com>
2020-01-30 08:11:32 -0800

    removed: login passport strat

--- src/util/configurePassport.ts
+++ src/util/configurePassport.ts
@@ -4,36 +4,9 @@ import {
   StrategyOptions,
   ExtractJwt
 } from "passport-jwt";
-import { Strategy as LocalStrategy } from "passport-local";
 import { PassportStatic } from "passport";
 import { getRepository } from "typeorm";
 import { User } from "../entities";
-
-// const verifyPassword = async (user: User, password: string): Promise<Boolean> =>
-//   await compare(password, user.password);
-
-// export const configurePassportLocal = (passport: PassportStatic) => {
-//   const userRepository = getRepository(User);
-//   passport.use(
-//     new LocalStrategy({ usernameField: "email" }, (email, password, done) => {
-//       userRepository
-//         .findOne({ email })
-//         .then(user => {
-//           if (!user) return done(null, false);
-//           verifyPassword(user, password)
-//             .then(isValid => {
-//               return isValid ? done(null, user) : done(null, false);
-//             })
-//             .catch(error => {
-//               return done(error);
-//             });
-//         })
-//         .catch(error => {
-//           return done(error);
-//         });
-//     })
-//   );
-// };
 
 export const configurePassportJwt = (passport: PassportStatic) => {
   const options: StrategyOptions = {
