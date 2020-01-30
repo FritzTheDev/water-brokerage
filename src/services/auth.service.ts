afd3eca9ef8d6dc4ebc0dd0679b2229cfbf20d22
Frederick Johnson <fritzj96@gmail.com>
2020-01-30 08:44:53 -0800

    added: auth service progress on the register function

--- src/services/auth.service.ts
+++ src/services/auth.service.ts
@@ -1,10 +1,10 @@
 import { getRepository } from "typeorm";
 import * as jwt from "jsonwebtoken";
-import { compare } from "bcrypt";
+import { compare, hash } from "bcrypt";
 
 import { LoginDTO, CreateUserDTO } from "../dtos";
 import { User } from "../entities";
-import { BadCredentialsException,  } from "../exceptions";
+import { BadCredentialsException, EmailInUseException } from "../exceptions";
 
 const createToken = (user: User) => {
   const id = user.id;
@@ -30,6 +30,17 @@ export const login = async (userCreds: LoginDTO) => {
 export const register = async (userData: CreateUserDTO) => {
   const userRepo = getRepository(User);
 
-  const user = await userRepo.findOne({ email: userData.email });
-  if (user) throw new EmailInUseException();
+  const userHasEmail = await userRepo.findOne({ email: userData.email });
+  if (userHasEmail) throw new EmailInUseException();
+  try {
+    userData.password = await hash(userData.password, 10);
+
+    const { fullName, email, password } = userData
+    const user = userRepo.create({ fullName, email, password });
+    await userRepo.save(user);
+    
+    return Promise.resolve(createToken(user));
+  } catch (error) {
+    throw error;
+  }
 }
