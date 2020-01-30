5f25fbcdb42390e7b6be3cce82a4832434a84d4c
Frederick Johnson <fritzj96@gmail.com>
2020-01-30 08:32:23 -0800

    fixed: loginhandler import/export

---
+++ src/controllers/auth.controller.ts
@@ -0,0 +1,28 @@
+import { Response, Request, NextFunction } from "express";
+import { login } from "../services";
+import { LoginDTO, CreateUserDTO } from "../dtos";
+
+export const loginHandler = async (
+  request: Request,
+  response: Response,
+  next: NextFunction
+) => {
+  const userCreds: LoginDTO = request.body;
+  try {
+    const token = await login(userCreds);
+    response.status(200).json({ token });
+  } catch (error) {
+    next(error);
+  }
+};
+
+export const registrationHandler = async (
+  request: Request,
+  response: Response,
+  next: NextFunction
+) => {
+  const userData: CreateUserDTO = request.body;
+  try {
+    const token = await register
+  }
+};
--- src/routes/auth.routes.ts
+++ src/routes/auth.routes.ts
@@ -1,13 +1,13 @@
 import Router from "express-promise-router";
 import { authenticate } from "passport";
-import { login } from "../controllers";
+import { loginHandler } from "../controllers";
 import { validationMiddleware } from "../middleware/validation.middleware";
 import { LoginDTO } from "../dtos/login.dto";
 import { CreateUserDto } from "../dtos/createUser.dto";
 
 const router = Router();
 
-router.post('/login',  validationMiddleware(LoginDTO), login);
+router.post('/login',  validationMiddleware(LoginDTO), loginHandler);
 router.post('/register', validationMiddleware(CreateUserDto),  )
 
 export { router as authRoutes };
