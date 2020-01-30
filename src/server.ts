ad609031fdb8d7f40165913a5d6d142d14dc92d1
Frederick Johnson <fritzj96@gmail.com>
2020-01-29 22:39:29 -0800

    added: comments to server.ts

--- src/server.ts
+++ src/server.ts
@@ -9,32 +9,48 @@ import { createConnection } from "typeorm";
 
 import { ormConfig } from "./util/ormConfig";
 import { validateEnv } from "./util/validateEnv";
+import { configurePassport } from "./util/configurePassport";
 import { authRoutes, userRoutes, westlandsAccountRoutes, listingRoutes } from "./routes";
 
-
+// top-level function
 const main = async () => {
+  // validates the presence env variables 
   validateEnv();
+
   // Don't need try/catch because if this throws the app is useless anyway
   const connection = await createConnection(ormConfig);
+
+  // run migrations on the database
   await connection.runMigrations();
 
+  // create app instance
   const app = express();
 
-  //middleware
+  // middleware
+  // cross origin resource sharing
+  // TODO: configure to only work with selected origins
   app.use(cors());
+  //logging
   app.use(morgan("common"));
+  // security stuff (?)
   app.use(helmet());
+  // body-parsing
   app.use(express.json());
+  // passport initialization & config
   app.use(passport.initialize());
+  configurePassport(passport);
 
+  // routers
   app.use('/auth', authRoutes);
   app.use('/users', userRoutes);
   app.use('/westlands-accounts', westlandsAccountRoutes);
   app.use('/listings', listingRoutes);
 
+  // start the server
   app.listen(process.env.PORT, () => {
     console.log(`Server is listening on port: ${process.env.PORT}`);
   });
 }
 
+// called to start the server
 main();
