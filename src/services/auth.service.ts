4137212a47a25e3d08ef55d13c1ecc690558bad2
Frederick Johnson <fritzj96@gmail.com>
2020-01-30 08:53:23 -0800

    refactored: login() return into the try block

--- src/services/auth.service.ts
+++ src/services/auth.service.ts
@@ -8,24 +8,25 @@ import { BadCredentialsException, EmailInUseException } from "../exceptions";
 
 const createToken = (user: User) => {
   const id = user.id;
-  return `Bearer ${jwt.sign({ id }, process.env.JWT_SECRET, { expiresIn: 60 * 60 * 24 * 7 })}`;
-}
+  return `Bearer ${jwt.sign({ id }, process.env.JWT_SECRET, {
+    expiresIn: 60 * 60 * 24 * 7
+  })}`;
+};
 
 export const login = async (userCreds: LoginDTO) => {
   const userRepo = getRepository(User);
-  
+
   const user = await userRepo.findOne({ email: userCreds.email });
   if (!user) throw new BadCredentialsException();
   try {
     if (!(await compare(userCreds.password, user.password))) {
       throw new BadCredentialsException();
     }
+    return Promise.resolve(createToken(user));
   } catch (error) {
     throw error;
   }
-  user.password = undefined;
-  return Promise.resolve(createToken(user));
-}
+};
 
 export const register = async (userData: CreateUserDTO) => {
   const userRepo = getRepository(User);
@@ -35,12 +36,12 @@ export const register = async (userData: CreateUserDTO) => {
   try {
     userData.password = await hash(userData.password, 10);
 
-    const { fullName, email, password } = userData
+    const { fullName, email, password } = userData;
     const user = userRepo.create({ fullName, email, password });
     await userRepo.save(user);
-    
+
     return Promise.resolve(createToken(user));
   } catch (error) {
     throw error;
   }
-}
+};
