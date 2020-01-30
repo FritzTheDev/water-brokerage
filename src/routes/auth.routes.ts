d87c79595c352a5d6202d01f706897cbe81595cb
Frederick Johnson <fritzj96@gmail.com>
2020-01-30 09:48:08 -0800

    added: registrationHandler to auth routes

--- src/routes/auth.routes.ts
+++ src/routes/auth.routes.ts
@@ -1,11 +1,11 @@
 import Router from "express-promise-router";
-import { loginHandler } from "../controllers";
+import { loginHandler, registrationHandler } from "../controllers";
 import { validationMiddleware } from "../middleware/validation.middleware";
 import { CreateUserDTO, LoginDTO } from "../dtos";
 
 const authRoutes = Router();
 
 authRoutes.post('/login',  validationMiddleware(LoginDTO), loginHandler);
-authRoutes.post('/register', validationMiddleware(CreateUserDTO),  )
+authRoutes.post('/register', validationMiddleware(CreateUserDTO), registrationHandler);
 
 export { authRoutes };
