93448da0347c571ddad040890e85261d6ed038ab
Frederick Johnson <fritzj96@gmail.com>
2020-01-30 05:53:36 -0800

    added: offerEntity

---
+++ src/entities/offer.entity.ts
@@ -0,0 +1,33 @@
+import { Entity, ManyToOne, Column, PrimaryGeneratedColumn, CreateDateColumn } from "typeorm";
+import { Listing } from "./listing.entity";
+import { User } from "./user.entity";
+
+@Entity()
+export class Offer {
+  @PrimaryGeneratedColumn()
+  public id: string;
+
+  @CreateDateColumn("date")
+  public createdDate: Date;
+
+  @Column("integer")
+  public offerPrice: Number;
+
+  @Column("integer")
+  public requestedVolume: Number;
+
+  @Column("date")
+  public availableDate: Date;
+
+  @ManyToOne(
+    () => Listing,
+    (listing: Listing) => listing.offers
+  )
+  public listing: Listing;
+
+  @ManyToOne(
+    () => User,
+    (user: User) => user.offers
+  )
+  public owner: User;
+}
